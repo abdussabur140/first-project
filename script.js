16ddf1ebc86cae0aaf59bb2be4a3c6ff941a0161
abdussabur140 <sharul454647@gmail.com>
2025-12-01 01:36:58 +0600

    Fixed collaps icon button

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
+
 const headers = document.querySelectorAll(".accordion-header");
 
-// Open first item by default
 if (headers.length > 0) {
     const firstBody = headers[0].nextElementSibling;
     const firstIcon = headers[0].querySelector(".icon");
 
-    firstBody.style.display = "block"; 
-    firstIcon.style.backgroundImage = 'url("minus-sign.svg")'; 
+    firstBody.style.display = "block";
+    firstIcon.style.backgroundImage = 'url("minus-sign.svg")';
 }
 
 headers.forEach(header => {
-    header.addEventListener("click", () => {
-
-        // Close all except clicked one
-        document.querySelectorAll(".accordion-body").forEach(body => {
-            if(body !== header.nextElementSibling) {
-                body.style.display = "none";
-            }
-        });
-
-        // Reset all icons except clicked header
-        document.querySelectorAll(".icon").forEach(icon => {
-            if(icon !== header.querySelector(".icon")) {
-                icon.style.backgroundImage = 'url("plus-sign (1).svg")';
-            }
-        });
+    const icon = header.querySelector(".icon");
+    icon.addEventListener("click", (e) => {
+        e.stopPropagation();   
 
         const body = header.nextElementSibling;
-        const icon = header.querySelector(".icon");
 
-        // Toggle
+        headers.forEach(h => {
+            if (h !== header) {
+                h.nextElementSibling.style.display = "none";
+                h.querySelector(".icon").style.backgroundImage =
+                    'url("plus-sign (1).svg")';
+            }
+        });
         if (body.style.display === "block") {
             body.style.display = "none";
             icon.style.backgroundImage = 'url("plus-sign (1).svg")';
@@ -37,5 +30,5 @@ headers.forEach(header => {
             body.style.display = "block";
             icon.style.backgroundImage = 'url("minus-sign.svg")';
         }
-    });
+    }); 
 });
